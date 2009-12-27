c0e5561e7c24ed8398226324f545f136ff51f5b9
Erik Grinaker <erikg@codepoet.no>
2009-12-27 10:03:23 +0700

    use utf-8 encoding for all python files

--- chrono/date.py
+++ chrono/date.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # python-chrono - a date/time module for python
 #
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # python-chrono - a date/time module for python
 #
