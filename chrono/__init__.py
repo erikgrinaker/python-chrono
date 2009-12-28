001e0b5ff1e7034ff93835afcd7482b3790b53f2
Erik Grinaker <erikg@codepoet.no>
2009-12-28 18:25:44 +0700

    changed module structure for parsers

--- chrono/__init__.py
+++ chrono/__init__.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from . import parser
+
 from .calendar import ISOCalendar
 from .date import Date
-from .parser import ISOParser
