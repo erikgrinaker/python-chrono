3e3fdab06b38fe646c8687f01fb1ca5096fee77d
Erik Grinaker <erikg@codepoet.no>
2010-03-09 00:25:27 +0700

    prepare version 0.3.0

--- chrono/__init__.py
+++ chrono/__init__.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-__version_info__ = ("0", "2", "0")
+__version_info__ = ("0", "3", "0")
 __version__ = ".".join(__version_info__)
 
 from . import calendar
